d4a1723801c266e0e041e293daca051cc6436685
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-29 00:37:33 +0100

    add cloudinary in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@nuxt/icon'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@nuxt/icon', "@nuxtjs/cloudinary"],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
