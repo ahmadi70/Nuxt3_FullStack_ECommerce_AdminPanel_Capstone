1f36f10a8da3f1201b437d6434d3a1f433e26d47
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-29 00:38:45 +0100

    install auth-utils for auth and session management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@nuxt/icon', "@nuxtjs/cloudinary"],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/icon',
+    "@nuxtjs/cloudinary",
+    'nuxt-auth-utils'
+  ],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
