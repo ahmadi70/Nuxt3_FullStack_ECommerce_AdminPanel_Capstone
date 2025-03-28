527490125aad300098aff3ed50b8a737963c5b0a
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-29 00:53:59 +0100

    setup runtimeConfig for git, strip and cloudinary

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,23 @@ export default defineNuxtConfig({
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
+  },
+  runtimeConfig: {
+    githubId: '',
+    githubSecret: '',
+    stripeSecret: '',
+    stripeWebhookSecret: '',
+    public: {
+      cloudinaryCloudName: '',
+      uploadPreset: '',
+      stripeKey: '',
+    },
+    cloudinaryApiKey: '',
+    cloudinaryApiSecret: '',
+  },
+  cloudinary: {
+    cloudName: process.env.NUXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    uploadPreset: process.env.NUXT_PUBLIC_UPLOAD_PRESET,
+    apiKey: process.env.NUXT_CLOUDINARY_API_KEY,
   }
 })
