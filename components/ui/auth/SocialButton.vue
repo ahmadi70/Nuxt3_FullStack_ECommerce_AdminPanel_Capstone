e027b09f2df40abd93782afdbe49b5dd304f78fd
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-29 02:25:13 +0100

    create reusable SocialButton component

---
+++ components/ui/auth/SocialButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  icon: string
+}>()
+</script>
+
+<template>
+  <Button type="button" variant="outline">
+    <div class="flex items-center gap-1">
+      <Icon :name="icon" class="w-4 h-4"></Icon>
+      {{ label }}
+    </div>
+  </Button>
+</template>
