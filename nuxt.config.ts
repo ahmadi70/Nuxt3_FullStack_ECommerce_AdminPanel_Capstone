b9724e811e16a0323567a86b97810907658982d0
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-29 03:06:31 +0100

    fix auto import components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  components: {
+    dirs: [
+      {
+        path: '~/components', 
+        pathPrefix: false,
+      }
+    ],
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     'shadcn-nuxt',
