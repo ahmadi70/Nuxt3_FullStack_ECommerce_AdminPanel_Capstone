3084c02de8ec10cff007534866d9666e9791d52d
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-30 04:54:51 +0200

    protect url

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+definePageMeta({
+  middleware: 'auth'
+})
 
+const { user, clear } = useUserSession()
+
+const logout = async () => {
+  await clear()
+  navigateTo('/auth/login')
+}
 </script>
 
 <template>
   <NuxtLink to="/auth/login">Login</NuxtLink> | 
-  <NuxtLink to="/auth/register">Register</NuxtLink>
+  <NuxtLink to="/auth/register">Register</NuxtLink> | 
+  <NuxtLink to="/admin">Admin</NuxtLink> | 
+  
+  <button @click="logout">Logout</button>
   <div>
     Home Page
   </div>
+  <pre>{{ user }}</pre>
 </template>
