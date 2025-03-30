17f0856fdf2f7ed175785e7f6125f9d3633d4a74
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-30 04:52:34 +0200

    define authenticate for github social button

--- components/auth/SocialButton.vue
+++ components/auth/SocialButton.vue
@@ -3,10 +3,18 @@ defineProps<{
   label: string
   icon: string
 }>()
+
+const authenticate = () => {
+  window.location.href = '/api/auth/github'
+}
 </script>
 
 <template>
-  <Button type="button" variant="outline">
+  <Button
+    @click="authenticate"
+    type="button"
+    variant="outline"
+  >
     <div class="flex items-center gap-1">
       <Icon :name="icon" class="w-4 h-4"></Icon>
       {{ label }}
