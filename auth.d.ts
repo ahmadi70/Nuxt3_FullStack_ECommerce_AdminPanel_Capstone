58d58b7f58a012b832190d6293238d4161f58d12
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-30 04:51:44 +0200

    create interface for auth user

---
+++ auth.d.ts
@@ -0,0 +1,20 @@
+// auth.d.ts
+declare module '#auth-utils' {
+  interface User {
+    id: string
+    email: string
+    role: 'USER' | 'ADMIN'
+    name: string
+    avatarUrl: string | null
+  }
+
+  interface UserSession {
+    // Add your own fields
+  }
+
+  interface SecureSessionData {
+    // Add your own fields
+  }
+}
+
+export {}
