c2ebf2b4800cc74274637fd879b421aadea7770e
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-01 00:41:06 +0200

    create get api to get all categories in desc

---
+++ server/api/admin/categories/index.get.ts
@@ -0,0 +1,11 @@
+import db from "~/utils/db"
+
+export default defineEventHandler(async (event) => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  return categories
+})
