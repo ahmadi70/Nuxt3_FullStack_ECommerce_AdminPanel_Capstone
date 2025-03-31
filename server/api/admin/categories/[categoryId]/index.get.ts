f4efa21633b12ff80cfdb165af074ee206564486
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-31 23:39:26 +0200

    create get api for single category

---
+++ server/api/admin/categories/[categoryId]/index.get.ts
@@ -0,0 +1,11 @@
+import db from "~/utils/db"
+
+export default defineEventHandler(async (event) => {
+  const category = await db.category.findUnique({
+    where: {
+      id: event.context.params?.categoryId
+    }
+  })
+
+  return category
+})
