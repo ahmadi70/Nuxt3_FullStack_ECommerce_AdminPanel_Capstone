d6fae5f74492851dec3e20278a54fddbdf10c828
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-31 23:38:33 +0200

    create post api for category

---
+++ server/api/admin/categories/index.post.ts
@@ -0,0 +1,29 @@
+import db from "~/utils/db"
+import { categorySchema } from "~/utils/validations"
+
+export default defineEventHandler(async (event) => {
+  
+  await requireUserSession(event)
+
+  const session = await getUserSession(event)
+
+  if (session.user && session.user.role === 'ADMIN') {
+
+    const { name } = await readValidatedBody(event, (body) => categorySchema.parse(body))
+
+    const category = await db.category.create({
+      data: {
+        name,
+        userId: session.user.id
+      }
+    })
+
+    return category
+    
+  } else {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized. You don't have admin access"    
+    })
+  }
+})
