ee4c51acc1a96322913b4327def6ba0dd33b73f9
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-31 23:35:55 +0200

    define categories page for admin

---
+++ pages/admin/categories/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+  middleware: 'admin'
+})
+</script>
+
+<template>
+  <div class="flex-col">
+    <div class="flex-1 space-y-4 p-8 pt-6">
+      <Client></Client>
+    </div>
+  </div>
+</template>
