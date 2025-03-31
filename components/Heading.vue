c9a5112253ac8299ff238e17752c34f24cc93e9f
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-31 23:33:42 +0200

    create Heading component

---
+++ components/Heading.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+interface HeadingProps {
+  title: string
+  description: string
+}
+
+defineProps<HeadingProps>()
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <div>
+      <h2 class="text-3xl font-bold tracking-tight">{{ title }}</h2>
+      <p class="text-sm text-muted-foreground">{{ description }}</p>
+    </div>
+    <slot name="action"></slot>
+  </div>
+</template>
