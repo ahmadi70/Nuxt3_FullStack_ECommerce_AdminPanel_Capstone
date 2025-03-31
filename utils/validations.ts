ee43bc66ce40c216744b714394757e88f7e5d517
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-31 23:25:13 +0200

    define validation for categorySchema

--- utils/validations.ts
+++ utils/validations.ts
@@ -3,5 +3,9 @@ import { z } from 'zod'
 export const authSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
-  name: z.string().max(255).default('').optional()
+  name: z.string().min(1).max(255).default('').optional()
+})
+
+export const categorySchema = z.object({
+  name: z.string().min(1)
 })
