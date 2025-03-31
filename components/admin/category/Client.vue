bc5e98bf0634be797c58c38f5b317a024bc1ffb3
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-31 23:33:11 +0200

    create category component for admin

---
+++ components/admin/category/Client.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <Heading
+      description="Manage Categories"
+      title="Categories"
+    >
+    </Heading>
+    <NuxtLink to="/admin/categories/new">
+      <Button>
+        <Icon name="lucide:plus" class="mr-2 h-4 w-4"></Icon>
+        Add New
+      </Button>
+    </NuxtLink>
+  </div>
+</template>
