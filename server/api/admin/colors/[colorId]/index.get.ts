abf37fe2fa70661edec0e135142ddd3bc7660c10
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-01 23:59:19 +0200

    add get api for color

---
+++ server/api/admin/colors/[colorId]/index.get.ts
@@ -0,0 +1,11 @@
+import db from "~/utils/db"
+
+export default defineEventHandler(async (event) => {
+  const color = await db.color.findUnique({
+    where: {
+      id: event.context.params?.colorId
+    }
+  })
+
+  return color
+})
