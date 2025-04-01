de1e2477051cffa8b98ad4af71235550a7742cbd
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-02 01:54:33 +0200

    create post api size

---
+++ server/api/admin/sizes/index.post.ts
@@ -0,0 +1,30 @@
+import db from "~/utils/db"
+import { sizeSchema } from "~/utils/validations"
+
+export default defineEventHandler(async (event) => {
+  
+  await requireUserSession(event)
+
+  const session = await getUserSession(event)
+
+  if (session.user && session.user.role === 'ADMIN') {
+
+    const { name, value } = await readValidatedBody(event, (body) => sizeSchema.parse(body))
+
+    const size = await db.size.create({
+      data: {
+        name,
+        value,
+        userId: session.user.id
+      }
+    })
+
+    return size
+    
+  } else {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized. You don't have admin access"    
+    })
+  }
+})
