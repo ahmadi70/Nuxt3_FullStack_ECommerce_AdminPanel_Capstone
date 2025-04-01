57ea8df5fae5e206082064f1f7f47d1f7d56c223
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-01 18:19:33 +0200

    rename to CategoryClient

--- pages/admin/categories/index.vue
+++ pages/admin/categories/index.vue
@@ -8,7 +8,7 @@ definePageMeta({
 <template>
   <div class="flex-col">
     <div class="flex-1 space-y-4 p-8 pt-6">
-      <Client></Client>
+      <CategoryClient></CategoryClient>
     </div>
   </div>
 </template>
