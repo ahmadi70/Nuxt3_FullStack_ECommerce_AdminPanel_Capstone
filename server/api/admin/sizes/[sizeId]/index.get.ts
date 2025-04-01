8c132cfaa7fa61fde04a19f2d462e37ca1afa262
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-02 01:55:07 +0200

    create get api for size

---
+++ server/api/admin/sizes/[sizeId]/index.get.ts
@@ -0,0 +1,11 @@
+import db from "~/utils/db"
+
+export default defineEventHandler(async (event) => {
+  const size = await db.size.findUnique({
+    where: {
+      id: event.context.params?.sizeId
+    }
+  })
+
+  return size
+})
