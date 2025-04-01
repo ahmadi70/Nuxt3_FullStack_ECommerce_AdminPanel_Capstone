81b4a6399f85c7fa1e9d5c7e8d692b3ba3577458
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-02 01:56:44 +0200

    create client component for size

---
+++ components/admin/size/SizeClient.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { columns } from '~/components/admin/size/SizeColumn'
+
+const { data: sizes, status } = await useFetch('/api/admin/sizes', {
+  key: 'sizes',
+  transform: (sizes) => {
+    return sizes.map(item => {
+      return {
+        id: item.id,
+        name: item.name,
+        value: item.value,
+        createdAt: useDateFormat(new Date(item.createdAt), 'D. MMMM YYYY')
+      }
+    })
+  }
+})
+
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <Heading
+      description="Manage Sizes"
+      title="Sizes"
+    >
+    </Heading>
+    <NuxtLink to="/admin/sizes/new">
+      <Button>
+        <Icon name="lucide:plus" class="mr-2 h-4 w-4"></Icon>
+        Add New
+      </Button>
+    </NuxtLink>
+  </div>
+  <DataTable
+    :columns="columns"
+    v-if="status !== 'pending'"
+    :data="sizes ? sizes : []"
+    column-to-search="name"
+  ></DataTable>
+</template>
