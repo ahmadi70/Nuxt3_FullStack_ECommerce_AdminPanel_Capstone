b4f2743dedaf72c8533b18d94f11daabbe8891dc
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-03 23:29:08 +0200

    create api to get sing product

---
+++ server/api/admin/products/[productId]/index.get.ts
@@ -0,0 +1,17 @@
+import db from "~/utils/db"
+
+export default defineEventHandler(async (event) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: event.context.params?.productId
+    },
+    include: {
+      images: true,
+      category: true,
+      color: true,
+      size: true
+    }
+  })
+
+  return product
+})
