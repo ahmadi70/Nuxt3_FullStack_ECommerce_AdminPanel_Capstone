30b51d9e36044b2b678d023b19f50f6af58caabf
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-03 23:31:07 +0200

    create api to get all products

---
+++ server/api/admin/products/index.get.ts
@@ -0,0 +1,17 @@
+import db from "~/utils/db"
+
+export default defineEventHandler(async (event) => {
+  const products = await db.product.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    },
+    include: {
+      images: true,
+      category: true,
+      color: true,
+      size: true
+    }
+  })
+
+  return products
+})
