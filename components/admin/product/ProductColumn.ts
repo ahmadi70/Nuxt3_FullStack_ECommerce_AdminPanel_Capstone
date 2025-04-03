ea86159390ab1537cc788fa188eb5334434a5923
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-03 23:36:13 +0200

    create ProductColumn component

---
+++ components/admin/product/ProductColumn.ts
@@ -0,0 +1,65 @@
+import type { ColumnDef } from "@tanstack/vue-table"
+import ActionMenu from './ProductCellAction.vue'
+import ColorValue from "../color/ColorValue.vue"
+
+export interface Color {
+  id: string
+  name: string
+  price: string
+  category: string
+  size: string
+  color: string
+  siFeatured: boolean
+  isArchived: boolean
+  createdAt: string
+}
+
+export const columns: ColumnDef<Color>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Name'
+  },
+  {
+    accessorKey: 'isArchived',
+    header: 'Archived'
+  },
+  {
+    accessorKey: 'isFeatured',
+    header: 'Featured'
+  },
+  {
+    accessorKey: 'price',
+    header: 'Price'
+  },
+  {
+    accessorKey: 'category',
+    header: 'Category'
+  },
+  {
+    accessorKey: 'size',
+    header: 'Size'
+  },
+  {
+    accessorKey: 'color',
+    header: 'Color',
+    cell: ({ row }) => {
+      return h(ColorValue, {
+        colorCode: row.original.color
+      })
+    }
+  },
+  {
+    accessorKey: 'createdAt',
+    header: 'Created At'
+  },
+  {
+    id: 'actions',
+    enableHiding: false,
+    cell: ({ row }) => {
+      const product = row.original
+      return h('div', { class: 'relative' }, h(ActionMenu, {
+        product
+      }))
+    }
+  }
+]
