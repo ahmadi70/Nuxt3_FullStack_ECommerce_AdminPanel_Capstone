af9041ce40efb5c09f5363b9b849d7ec6222498b
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-04 22:37:43 +0200

    create FeaturedSection component

---
+++ components/home-sections/FeaturedSection.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { SafeProduct } from '~/types'
+
+const { data, status } = await useFetch<{ featuredProducts: SafeProduct[] }>('/api/admin/products', {
+  lazy: true
+})
+
+const featuredProducts = computed(() => data.value?.featuredProducts || [])
+
+</script>
+
+<template>
+  <Section>
+    <ProductList :items="featuredProducts" title="Featured Products" :is-loading="status === 'pending'" />
+  </Section>
+</template>
