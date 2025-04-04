bc3d16a58d267a71ddff0840cfa980faf8a88cd4
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-04 22:38:42 +0200

    create NoResult component

---
+++ components/NoResult.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-full  w-full space-y-3">
+    <p class="text-neutral-500">No results found.</p>
+    <NuxtLink to="/products">
+      <Button variant="outline">Clear Filters</Button>
+    </NuxtLink>
+  </div>
+</template>
