cac1ea8ed2a5ddc5ade91c1fdc6367248207d4b4
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-04 22:38:02 +0200

    create CardLoader component

---
+++ components/CardLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <Card>
+    <CardContent class="p-0">
+      <div class="grid gap-4 p-4 pt-4 w-full">
+        <div class="overflow-hidden rounded-md relative">
+          <Skeleton class="h-[170px] transition-all hover:scale-105 bg-foreground/20" />
+            <div class="flex flex-col space-y-2 absolute top-2 right-2">
+              <Skeleton class="size-8 rounded-full" />
+              <Skeleton class="size-8 rounded-full" />
+            </div>
+        </div>
+        <Skeleton class="h-4 w-[90%] bg-foreground/20" />
+        <Skeleton class="h-4 w-[80%] bg-foreground/20" />
+      </div>
+    </CardContent>
+    <CardFooter class="border-t p-4 pt-4 w-full justify-between">
+      <Skeleton class="h-4 w-[200px] bg-foreground/20" />
+    </CardFooter>
+  </Card>
+</template>
