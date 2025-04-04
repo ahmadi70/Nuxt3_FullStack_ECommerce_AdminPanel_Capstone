86d2d6b49e0c0c333e576b0cf9785b1da1821731
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-04 22:36:48 +0200

    create Section component

---
+++ components/home-sections/Section.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <div class="mx-auto w-full max-w-5xl py-16 sm:py-32">
+        <div class="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <slot></slot>
+        </div>
+    </div>
+  </section>
+</template>
