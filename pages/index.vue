4213ba5f68bca64d09e9d2b16e960e57734eee11
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-04 22:35:42 +0200

    design homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,6 @@
-<script setup lang="ts">
-definePageMeta({
-  middleware: 'auth'
-})
-
-const { user, clear } = useUserSession()
-
-const logout = async () => {
-  await clear()
-  navigateTo('/auth/login')
-}
-</script>
-
 <template>
-  <NuxtLink to="/auth/login">Login</NuxtLink> | 
-  <NuxtLink to="/auth/register">Register</NuxtLink> | 
-  <NuxtLink to="/admin">Admin</NuxtLink> | 
-  <NuxtLink to="/admin/categories">Categories</NuxtLink> | 
-  <NuxtLink to="/admin/colors">Colors</NuxtLink> | 
-  <NuxtLink to="/admin/sizes">Sizes</NuxtLink> | 
-  <NuxtLink to="/admin/products">Product</NuxtLink> | 
-  
-  <button @click="logout">Logout</button>
   <div>
-    Home Page
+    <HeroSection></HeroSection>
+    <FeaturedSection></FeaturedSection>
   </div>
-  <pre>{{ user }}</pre>
 </template>
