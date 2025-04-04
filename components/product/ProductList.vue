67ad15cf04d66f85be2afa7fb11574a5ada41509
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-04 22:36:16 +0200

    create ProductList component

---
+++ components/product/ProductList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { SafeProduct } from '~/types'
+
+defineProps<{
+  title: string
+  items: SafeProduct[]
+  isLoading: boolean
+}>()
+</script>
+
+<template>
+  <div class="space-y-4">
+    <h3 class="font-bold text-3xl">{{ title }}</h3>
+    <NoResult v-if="!items.length"></NoResult>
+    <div class="grid grid-cols-3 gap-8">
+      <template v-if="!isLoading">
+        <ProductCard v-for="item in items" :key="item.id" :data="item"></ProductCard>
+      </template>
+      <template v-else>
+        <CardLoader v-for="i in 3" :key="i"></CardLoader>
+      </template>
+    </div>
+  </div>
+</template>
