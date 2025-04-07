84b19c37bef2c6c83543bfb22f8e310e2949fe04
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-07 22:47:56 +0200

    create MobileFilter component

---
+++ components/MobileFilter.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Color, Size, Category } from '@prisma/client'
+
+interface FilterProps {
+  sizes: Size[]
+  colors: Color[]
+  categories: Category[]
+}
+
+defineProps<FilterProps>()
+
+</script>
+
+<template>
+  <Sheet>
+    <SheetTrigger>
+      <Button class="rounded-full lg:hidden">
+        <Icon class="size-4 mr-2" name="lucide:plus" />
+        Filters
+      </Button>
+    </SheetTrigger>
+    <SheetContent>
+      <SheetHeader class="mb-6">
+        <SheetTitle>Filters</SheetTitle>
+      </SheetHeader>
+      <Filter value-key="sizeId" name="Sizes" :data="sizes" />
+      <Filter value-key="colorId" name="Colors" :data="colors" />
+      <Filter value-key="categoryId" name="Categories" :data="categories" />
+    </SheetContent>
+  </Sheet>
+</template>
