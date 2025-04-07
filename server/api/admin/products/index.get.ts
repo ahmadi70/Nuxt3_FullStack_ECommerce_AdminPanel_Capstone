ca263ff95c3c5edb540a30817de3bb7a27e66ffd
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-07 22:51:18 +0200

    update api get products

--- server/api/admin/products/index.get.ts
+++ server/api/admin/products/index.get.ts
@@ -1,6 +1,33 @@
 import db from "~/utils/db"
 
 export default defineEventHandler(async (event) => {
+
+  const query = getQuery(event)
+
+  const categoryId = query.categoryId
+  const colorId = query.colorId
+  const sizeId = query.sizeId
+  const isFeatured = query.isFeatured
+  const isArchived = query.isArchived
+
+  const queryParam: any = {}
+
+  if (categoryId) {
+    queryParam.categoryId = categoryId
+  }
+  if (colorId) {
+    queryParam.colorId = colorId
+  }
+  if (sizeId) {
+    queryParam.sizeId = sizeId
+  }
+  if (isFeatured) {
+    queryParam.isFeatured = !!isFeatured
+  }
+  if (isArchived) {
+    queryParam.isArchived = !!isArchived
+  }
+
   const products = await db.product.findMany({
     orderBy: {
       createdAt: 'desc'
@@ -10,43 +37,9 @@ export default defineEventHandler(async (event) => {
       category: true,
       color: true,
       size: true
-    }
+    },
+    where: queryParam
   })
 
-  const featuredProducts = await db.product.findMany({
-    where: {
-      isFeatured: true,
-      isArchived: false,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: {
-      images: true,
-      category: true,
-      color: true,
-      size: true,
-    },
-  })
-
-  const archivedProducts = await db.product.findMany({
-    where: {
-      isArchived: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: {
-      images: true,
-      category: true,
-      color: true,
-      size: true,
-    },
-  })
-
-  return {
-    products,
-    featuredProducts,
-    archivedProducts,
-  }
+  return products
 })
