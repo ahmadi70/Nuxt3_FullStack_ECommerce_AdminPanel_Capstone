eea34c63721f2733ae6fb919810f8eba98d8083f
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-08 08:13:34 +0200

    update to fetch featured products

--- components/home-sections/FeaturedSection.vue
+++ components/home-sections/FeaturedSection.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 import type { SafeProduct } from '~/types'
 
-const { data, status } = await useFetch<{ featuredProducts: SafeProduct[] }>('/api/admin/products', {
+const { data: products, status } = await useFetch<SafeProduct[]>(`/api/admin/products?isFeatured=true`, {
   lazy: true
 })
-
-const featuredProducts = computed(() => data.value?.featuredProducts || [])
 
 </script>
 
 <template>
   <Section>
-    <ProductList :items="featuredProducts" title="Featured Products" :is-loading="status === 'pending'" />
+    <ProductList :items="products" title="Featured Products" :is-loading="status === 'pending'" />
   </Section>
 </template>
