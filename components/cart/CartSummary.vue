2e22d97a81096f51f8b563703a6fccc02b671700
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-08 12:35:38 +0200

    create CartSummary component

---
+++ components/cart/CartSummary.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import useCart from '~/compsables/useCart'
+
+const { cartItems, removeAll} = useCart()
+
+const totalPrice = computed(() => {
+  return cartItems.value.reduce((total, item) => {
+    return total + Number(item.price)
+  }, 0)
+})
+
+const haveItem = computed(() => cartItems.value.length)
+
+const onCheckout = async () => {
+  const orderItems = cartItems.value.map(item => item.id)
+
+  const data = await $fetch('/api/checkout', {
+    method: 'POST',
+    body: orderItems
+  })
+
+  console.log(data)
+
+  removeAll()
+
+}
+</script>
+
+<template>
+  <div class="lg:ml-16 rounded-lg bg-gray-50 px-4 py-6 lg:col-span-5 lg:mt-0 lg:p-8">
+    <h2 class="text-lg font-medium text-gray-900">Order summary for this</h2>
+    <div class="mt-6 space-y-4">
+      <div class="flex items-center justify-between border-t border-gray-200 pt-4">
+        <div class="text-base font-medium text-gray-900">Order total</div>
+        <div class="font-semibold">{{ formatter.format(Number(totalPrice)) }}</div>
+      </div>
+    </div>
+    <Button
+      @click="onCheckout"
+      type="button"
+      :disabled="!haveItem"
+      class="w-full mt-6"      
+    >Checkout</Button>
+  </div>
+</template>
