682c9beb6e7e7567fc5f3a4ce2211d541f471d97
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-08 12:33:05 +0200

    define get & set functions for data storgae

--- utils/index.ts
+++ utils/index.ts
@@ -6,3 +6,24 @@ export const formatter = new Intl.NumberFormat('en-us', {
   style: 'currency',
   currency: 'USD'
 })
+
+export function getLocalstorageData<T>(key: string) {
+  if (import.meta.client) {
+    const data = window.localStorage.getItem(key)
+    if (data) {
+      return JSON.parse(data) as T
+    }
+    return null as T
+  }
+  return null as T
+}
+
+export function setStorageData<T>(key: string, data: T | null) {
+  if (import.meta.client) {
+    if (data) {
+      window.localStorage.setItem(key, JSON.stringify(data))
+    } else {
+      window.localStorage.removeItem(key)
+    }
+  }
+}
