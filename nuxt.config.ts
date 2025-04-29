d367922b59b3b472047e0766943bc3397fd0ab57
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-04-30 00:27:37 +0200

    improve: enable the pathPrefix to default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,6 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  components: {
-    dirs: [
-      {
-        path: '~/components', 
-        pathPrefix: false,
-      }
-    ],
-  },
   modules: [
     '@nuxtjs/tailwindcss',
     'shadcn-nuxt',
